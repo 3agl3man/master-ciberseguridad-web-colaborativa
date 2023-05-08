cb552449fa05c9e6e89e9d0444b0e1a49ec28263
Javi Espinosa <javier.espinosagar@gmail.com>
2023-05-08 18:39:04 +0200

    fix: Add password policy

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -7,17 +7,29 @@ import play.mvc.Controller;
 
 public class PublicContentBase extends Controller {
 
+    public static final int MIN_PASSWORD_LENGTH = 10;
+
     public static void register(){
         render();
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+        if (validatePassword(password)){
+            User u = new User(username, HashUtils.getMd5(password), type, -1);
+            u.save();
+            registerComplete();
+        }else {
+            flash.put("pwlen", "Password should be at least 10 characters.");
+            register();
+        }
     }
 
     public static void registerComplete(){
         render();
+    }
+
+
+    private static boolean validatePassword(String password){
+        return password.length() >= MIN_PASSWORD_LENGTH;
     }
 }
